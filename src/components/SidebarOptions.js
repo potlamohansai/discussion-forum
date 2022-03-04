e2492f6ebea8ac68d7d0aede03f77baf7759a612
Mohan Sai Potla <potlamohansai@gmail.com>
2022-03-04 23:55:18 +0530

    sidebar

--- src/components/SidebarOptions.js
+++ src/components/SidebarOptions.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SidebarOptions() {
   return (
     <div>
-        hello from sidebar options section page and page
+        hello from sidebar options section page
     </div>
   )
 }
