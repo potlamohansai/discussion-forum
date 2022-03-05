0e5d5464ad92afa6bb9cfb588ab9b8e8afda04ce
Mohan Sai Potla <potlamohansai@gmail.com>
2022-03-05 09:40:51 +0530

    posts

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import WidgetContent from './WidgetContent'
+import './css/Widget.css'
 
 function Widget() {
   return (
-    <div>
-        From Widget
+    <div className='widget'>
+      <div className='widget__header'>
+        <h5>Spaces to Follow</h5>
+        </div>
+        <div className='widget__contents'>
+          <WidgetContent/>
+        </div>
+      
     </div>
   )
 }
---
+++ src/components/WidgetContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './css/WidgetContent.css'
+
+function WidgetContent() {
+  return (
+    <div className='widget__contents'>
+        <div className='widget__content'>
+            <img src='https://moodle.com/wp-content/uploads/2019/04/BrandedAppIcon_Publishing-and-maintenance.png' alt='' />
+            <div className='widget__contentTitle'>
+                <h5>Mobile App Programmers</h5>
+                <p> The best Mobile app Development Company</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WidgetContent
